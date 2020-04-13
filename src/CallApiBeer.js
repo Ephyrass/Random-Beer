5417a2c75074644117e188bbf087a51266b43546
ephyrass <jb.houi@yahoo.fr>
2020-04-13 17:40:31 +0200

    update to useReducer

--- src/CallApiBeer.js
+++ src/CallApiBeer.js
@@ -1,38 +1,72 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
+import axios from "axios";
+
+const initialState = {
+  beer: {},
+  loading: true,
+  error: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "succes":
+      return {
+        beer: action.payload,
+        loading: false,
+      };
+    case "failed":
+      return {
+        error: "There is an error, try again.",
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
 
 const CallApiBeer = () => {
-  const [beerData, setBeerData] = useState({});
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const fetchData = async () => {
-    const res = await fetch(`https://api.punkapi.com/v2/beers/random`);
-    const data = await res.json();
-    setBeerData(data[0]);
+  const fetchData = () => {
+    axios(`https://api.punkapi.com/v2/beers/random`)
+      .then((res) => {
+        dispatch({ type: "succes", payload: res.data[0] });
+      })
+      .catch((error) => {
+        dispatch({ type: "failed" });
+      });
   };
 
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(beerData);
+
+  console.log(state.beer);
   return (
     <div>
-      {beerData.image_url === null ? (
-        <h2>No picture :(</h2>
+      {state.loading ? (
+        "Loading..."
       ) : (
-        <img
-          style={{ height: "400px" }}
-          src={beerData.image_url}
-          alt={beerData.name}
-        />
+        <div>
+          {state.beer.image_url === null ? (
+            <h2>No picture :(</h2>
+          ) : (
+            <img
+              style={{ height: "400px" }}
+              src={state.beer.image_url}
+              alt={state.beer.name}
+            />
+          )}
+          <h2>{state.beer.name}</h2>
+          <p style={{ width: "50%", margin: "auto", padding: "0 5%" }}>
+            {state.beer.description}
+          </p>
+          <button className='discover' onClick={() => fetchData()}>
+            Discover
+          </button>
+        </div>
       )}
-      <h2>{beerData.name}</h2>
-
-      <p style={{ width: "50%", margin: "auto", padding: "0 5%" }}>
-        {beerData.description}
-      </p>
-
-      <button className="discover" onClick={() => fetchData()}>
-        Discover
-      </button>
+      {state.error ? state.error : null}
     </div>
   );
 };
